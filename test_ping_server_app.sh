516e44d195bf5ec3d837ac1f8230ed8839d5126c
Anna Slastnikova <anna.slastnikova@gmail.com>
2016-08-04 17:12:37 +0100

    add test bash script

---
+++ test_ping_server_app.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+hostName="localhost:8080"
+outputFileName="curlResult.txt"
+
+data="Hello, world!"
+echo `curl -s "$hostName" > $outputFileName`
+
+response="$(cat "$outputFileName")"
+
+if [ "$response" = "$data" ]; then
+    echo "Success!"
+else
+    echo "Fail!"
+fi
+
