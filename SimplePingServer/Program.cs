adef02677c844292c362b16bb004b894de968915
Anna Slastnikova <anna.slastnikova@gmail.com>
2016-08-04 15:21:17 +0100

    Proper server

--- SimplePingServer/Program.cs
+++ SimplePingServer/Program.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Timers;
 
 namespace SimplePingServer
@@ -11,41 +12,105 @@ namespace SimplePingServer
 	class MainClass
 	{
 		const int PORT_NO = 8080;
-		const string SERVER_IP = "127.0.0.1";
-		const int LIMIT = 5;
 
-		static Timer aTimer;
+		// Thread signal
+		public static ManualResetEvent thread = new ManualResetEvent(false);
 
-		static void Main(string[] args)
+		public static void Listen()
 		{
-			aTimer = new Timer(1000);
-			aTimer.Elapsed += PingIt;
-			aTimer.AutoReset = true;
-			Console.WriteLine("The timer should fire every {0} milliseconds.",
-			    aTimer.Interval);
-			aTimer.Enabled = true;
+			// Establish the local endpoint for the socket.
+			IPHostEntry ipHostInfo = Dns.GetHostEntry("localhost");
+			IPAddress ipAddress = ipHostInfo.AddressList[0];
+			IPEndPoint localEndPoint = new IPEndPoint(ipAddress, PORT_NO);
 
-			Console.ReadLine();
+			// Create a TCP/IP socket.
+			Socket listener = new Socket(AddressFamily.InterNetwork,
+				SocketType.Stream, ProtocolType.Tcp);
+
+			try
+			{
+				// Bind the socket to the local endpoint
+				listener.Bind(localEndPoint);
+				//  and listen for incoming connections
+				listener.Listen(100);
+
+				while (true)
+				{
+					thread.Reset();
+
+					// Start an asynchronous socket to listen for connections.
+					Console.WriteLine("Waiting for a connection...");
+					listener.BeginAccept(
+						new AsyncCallback(AcceptCallback),
+						listener);
+
+					// Wait until a connection is made before continuing
+					thread.WaitOne();
+				}
+
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine("Can't connect to localhost: " + e.ToString());
+			}
+
+			Console.Read();
 		}
 
-		static void PingIt(Object source, ElapsedEventArgs e)
+		public static void AcceptCallback(IAsyncResult ar)
 		{
-			// listen at the specified IP
-			IPAddress localIP = IPAddress.Parse(SERVER_IP);
-			TcpListener listener = new TcpListener(localIP, PORT_NO);
-			Console.WriteLine("Listening...");
-			listener.Start();
+			thread.Set();
 
-			// incoming client connected
-			TcpClient client = listener.AcceptTcpClient();
+			Socket listener = (Socket)ar.AsyncState;
+			// socket which handles incoming connections
+			Socket handler = listener.EndAccept(ar);
 
-			string dataToSendBack = "Hello, world!";
-			// write back the text to the client
-			Console.WriteLine("Sending back : " + dataToSendBack);
-			client.Close();
-			listener.Stop();
+			byte[] buffer = new byte[1024];
+			// handler is starting to recieve data from the lestener
+			handler.BeginReceive(buffer, 0, 0, SocketFlags.None,
+			    new AsyncCallback(ReadCallback), handler);
+		}
 
-			Console.ReadLine();
+		public static void ReadCallback(IAsyncResult ar)
+		{
+			Socket handler = (Socket)ar.AsyncState; ;
+
+			string dataToSend = "Hello, world!\n";
+			Send(handler, dataToSend);
+		}
+
+		private static void Send(Socket handler, String data)
+		{
+			byte[] byteData = Encoding.ASCII.GetBytes(data);
+
+			// Begin sending the data to the client
+				handler.BeginSend(byteData, 0, byteData.Length, SocketFlags.None,
+					new AsyncCallback(SendCallback), handler);
+		}
+
+		private static void SendCallback(IAsyncResult ar)
+		{
+			try
+			{
+				Socket handler = (Socket)ar.AsyncState;
+
+				// When sending data to the client is finished:
+				Console.WriteLine("Said hello to client.");
+
+				handler.Shutdown(SocketShutdown.Both);
+				handler.Close();
+
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine("Couldn't connect to client: " + e.ToString());
+			}
+		}
+
+
+		public static void Main(String[] args)
+		{
+			Listen();
 		}
 	}
 }
