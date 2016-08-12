09f4df940377e7f542c27e39e142eb0610ce5c1c
as <anna.slastnikova@gmail.com>
2016-08-12 17:08:24 +0200

    Before testing remove containers and image

--- prepare_tests.sh
+++ prepare_tests.sh
@@ -1,13 +1,13 @@
-
-
 #!/bin/bash
 
-CONTAINERS=$(docker ps -q)
-if [ $CONTAINERS ] 
+CONTAINERS=$(docker ps -q -a)
+if [ "$CONTAINERS" ]
 then
 	docker stop $CONTAINERS &> /dev/null
+	docker rm $CONTAINERS &> /dev/null
 fi
 
-docker build -t app . &> /dev/null
-docker run -d -p 9000:9000 app &> /dev/null
+docker rmi app &> /dev/null
 
+docker build -t app . &> /dev/null
+docker run -p 9000:9000  app &
