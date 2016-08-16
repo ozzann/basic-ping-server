6d9fbbe68392a13ff1ef4a06216a99bf9097c9a6
Anna Slastnikova <anna.slastnikova@gmail.com>
2016-08-16 17:18:34 +0100

    resolve conflicts

--- run_tests.sh
+++ run_tests.sh
@@ -4,10 +4,6 @@
 # tmp file containing container ID
 export CIDFILE="app.cid"
 
-<<<<<<< HEAD
-echo "Test #1: check server's response................"
-./test_ping_server_app.sh
-=======
 echo "Preparing tests................................."
 ./prepare_tests.sh  
 
@@ -15,7 +11,6 @@ FULLCID=$(cat $CIDFILE)
 export CONTAINERID=${FULLCID:0:12}
 rm $CIDFILE
  ./test_ping_server_app.sh
->>>>>>> origin/master
 
 if [ $? = 1 ]
 then 
