af5615a14c2314504b6bad139a581c5655f9b9d6
Anna Slastnikova <anna.slastnikova@gmail.com>
2016-08-19 14:45:09 +0100

    output docker command run

--- deploy.sh
+++ deploy.sh
@@ -7,11 +7,7 @@ then
 	docker rm $CONTAINERS &> /dev/null
 fi
 
-docker build -t app . &> /dev/null
-if [ $? -ne 0 ]; then
-    echo 'Docker build failed'
-    exit 1
-fi
+docker build -t app . 
 
 docker run -d -p 9000:9000 app
 
